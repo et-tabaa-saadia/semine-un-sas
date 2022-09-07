a8d2528376240fccd4092a54582ca777a3b506fa
saadiatabaa <saadiatabaa123@gmail.com>
2022-09-07 16:48:11 +0100

    semine1

---
+++ challenge4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	float metre ,mile;
+	printf("entrer la distance en mile : ");
+	scanf("%f",&mile),
+	metre= mile*1609;
+	printf("la distance en metre %f" ,metre);
+	
+	
+	
+	
+}
