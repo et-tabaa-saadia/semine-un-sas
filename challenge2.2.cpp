4c7b857178c059d24bb9b129aac0d6b91a14cd84
saadiatabaa <saadiatabaa123@gmail.com>
2022-09-08 14:02:55 +0100

    challenge 2

---
+++ challenge2.2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int  nombre;
+	
+	printf("ectrer nu nombre : ");
+	scanf("%d",&nombre);
+	
+	if(nombre%2== 0)
+	{
+	printf ("le nombre paire");
+}
+  else 
+   printf ("le nombre inpaire");
+}
